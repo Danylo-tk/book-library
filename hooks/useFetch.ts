2696f0a8b3b2feeca948367dac8bf828984cf5b0
Danylo Tkach <danylo.tkach@gmail.com>
2023-06-10 23:25:28 +0300

    Custom hook useFetch

---
+++ hooks/useFetch.ts
@@ -0,0 +1,42 @@
+import { useEffect, useState } from "react";
+
+type FetchState<T> = {
+  data: T | null;
+  error: Error | null;
+  isLoading: boolean;
+};
+
+const useFetch = <T>(url: string): FetchState<T> => {
+  const [data, setData] = useState<T | null>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+
+      try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error("Request failed.");
+        }
+
+        const json = await response.json();
+        setData(json);
+      } catch (err) {
+        if (err instanceof Error) {
+          setError(err);
+        }
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, isLoading };
+};
+
+export default useFetch;
