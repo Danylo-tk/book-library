d30f0062a3b3aca55cf38812e59dff84a0bd1c49
Danylo Tkach <danylo.tkach@gmail.com>
2023-06-12 21:49:52 +0300

    Updated config files. Added custom color acidGreen, frosted-glass css style

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,9 @@ module.exports = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        acidGreen: "#90FE37",
+      },
     },
   },
   plugins: [],
