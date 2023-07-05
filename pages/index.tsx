079989342f69e631e8456e7717b15c6a54cc4b33
Danylo Tkach <danylo.tkach@gmail.com>
2023-07-05 23:28:39 +0300

    New layout of a home page
    Created a new layout, added windwe for registration, new blur style, google icon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,17 @@
+import { Button } from "@/components/Button";
+import { Loader } from "@/components/Loader";
 import { initFirebase } from "@/firebase/firebase";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
-import Link from "next/link";
+import Image from "next/image";
 import { useRouter } from "next/router";
 import { useAuthState } from "react-firebase-hooks/auth";
 
-export function Header() {
+export default function Home() {
   initFirebase();
   const auth = getAuth();
   const provider = new GoogleAuthProvider();
   const [user, loading] = useAuthState(auth);
   const router = useRouter();
-
-  if (loading) {
-    return <div>Loading...</div>;
-  }
 
   if (user) {
     router.push("/book-list");
@@ -21,79 +19,50 @@ export function Header() {
 
   const signIn = async () => {
     const result = await signInWithPopup(auth, provider);
-    console.log(result.user);
   };
 
   return (
-    <nav className="flex items-center justify-between border-b border-l-0 border-r-0 border-t-0 border-solid border-black py-5 ">
-      <span className="text-3xl">📚</span>
+    <main className="relative h-full bg-acidGreen">
+      <div className="frosted-glass-main absolute top-0 h-full w-full"></div>
 
-      <ul className="flex list-none items-center gap-4 text-xl uppercase sm:gap-7 sm:text-3xl">
-        <li className="no-underline">
-          <a
-            href="https://github.com/Danylo-tk/book-library"
-            className="text-black no-underline hover:text-acidGreen active:text-black"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <Link
-            href={"/book-list"}
-            className="text-black no-underline hover:text-acidGreen active:text-black"
-          >
-            List
-          </Link>
-        </li>
-        <li>
-          <Link
-            href={"/book-editor"}
-            className="text-black no-underline hover:text-acidGreen active:text-black"
-          >
-            Add
-          </Link>
-        </li>
-        <li>
-          <a onClick={signIn}>Sign in</a>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+      <h1 className="text-4xl sm:text-6xl lg:text-8xl">BOOKS LIBRARY</h1>
+      <h1 className="text-4xl sm:text-6xl lg:text-8xl">/LOGIN</h1>
 
-export default function Home() {
-  return (
-    <main className="h-full ">
-      <Header />
-      <div className="flex h-full flex-col items-center">
-        <div className="h-14"></div>
+      <div className="absolute top-0 flex h-full w-full items-center justify-center">
+        <div className="absolute z-10 flex h-56 w-72 flex-col justify-between border border-solid border-gray-400 bg-white p-2">
+          {loading ? (
+            <div className="flex h-full items-center justify-center">
+              <Loader size={48} />
+            </div>
+          ) : (
+            <>
+              <div>
+                <h2 className="text-xl font-bold">📚 BOOKS LIBRARY</h2>
+                <div>
+                  <p>* Add new books to the library</p>
+                  <p>* Update/delete book information</p>
+                  <p>* Set activity of a book</p>
+                </div>
+              </div>
 
-        <div className="">
-          <div className="flex justify-between text-[5vw] text-gray-300">
-            <span>l</span>
-            <span>i</span>
-            <span>b</span>
-            <span>r</span>
-            <span>a</span>
-            <span>r</span>
-            <span>y</span>
-          </div>
-          <h1 className=" text-[17vw] font-normal uppercase">
-            B<span className="text-violet-500">o</span>
-            <span className="text-orange-500">o</span>
-            <span className="text-blue-500">o</span>
-            <span className="text-green-500">o</span>
-            ks
-          </h1>
-        </div>
-
-        <div className="h-20 sm:h-14"></div>
-
-        <div>
-          <p className="text-center text-3xl">
-            *Unlock the magic of literature in Books library, the ultimate book
-            lover&apos;s sanctuary.*
-          </p>
+              <div className="flex flex-col">
+                <p className="text-2xl font-medium text-gray-600">
+                  /login to continue
+                </p>
+                <Button onClick={signIn}>
+                  <div className="flex w-full items-center justify-center gap-2">
+                    <Image
+                      src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                      height={30}
+                      width={30}
+                      alt="Google logo"
+                    />{" "}
+                    Continue with Google
+                  </div>
+                </Button>
+              </div>
+            </>
+          )}
         </div>
       </div>
     </main>
